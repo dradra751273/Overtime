f246baa232e0da32585dcb84d31a1435fc04b3dd
dradra751273 <dradra751273@gmail.com>
2024-08-17 15:08:05 +0800

    Refactor overtime.py

--- overtime.py
+++ overtime.py
@@ -1,187 +1,258 @@
 import pandas as pd
-from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows
-from openpyxl.styles import Font, Border, Side, Alignment
 
-adi_sort_key = {
-    "署長室": 1, "胡副署長室": 2, "林副署長室": 3, "主任秘書室": 4,
-    "政策規劃組": 5, "前瞻政策科": 6, "產業人才科": 7, "計畫管理科": 8, "綜合業務科": 9,
-    "通訊傳播組": 10, "基礎環境科": 11, "傳播推廣科": 12, "通訊應用科": 13,
-    "平臺經濟組": 14, "平臺應用科": 15, "平臺治理科": 16, "數位體驗科": 17, "數據經濟科": 18,
-    "新興跨域組": 19, "資安產業科": 20, "全球運籌科": 21, "場域實證科": 22, "地方鏈結科": 23,
-    "數位服務組": 24, "軟體產業科": 25, "轉型輔導科": 26, "整合服務科": 27, "創新應用科": 28,
-    "秘書室": 29, "文書科": 30, "事務科": 31,
-    "人事室": 32, "政風室": 33, "主計室": 34,
-    "署長": 35, "副署長": 36, "主任秘書": 37,
-    "組長": 38, "副組長": 39, "簡任技正": 40, "簡任視察": 41,
-    "科長": 42, "技正": 43, "視察": 44, "專員": 45, "科員": 46, "助理員": 47,
-    "專案規劃師": 48, "專案分析師": 49
-}
+import xlwings as xw
+from xlwings.constants import VAlign, HAlign
 
 
-def clean_data(source_df: pd.DataFrame):
-    """
-    :param source_df: source dataframe
-    :return: reformatted dataframe
-    """
-    ch_cols = ["單位名稱", "職稱", "姓名", "加班日期", "核可時數", "已請款時數", "已補休時數", "剩餘可用時數"]
-    en_cols = ["unit", "job", "name", "mons", "appr", "paid", "rest", "remain"]
-    result: pd.DataFrame = source_df.copy(deep=True)
-    result.columns = source_df.iloc[3]
-    result = result.iloc[4:][ch_cols].copy(deep=True)
-    result.rename(columns={ch_cols[i]: en_cols[i] for i in range(len(ch_cols))}, inplace=True)
-    result["mons"] = result["mons"].str.split("/").map(lambda x: str(int(x[1])))
-    # split hours and minutes
-    for col in en_cols[4:]:
-        result[col] = result.fillna("0-0")
-        result[f"{col}_H"] = result[col].str.split("-", expand=True).loc[:, 0].astype(int)
-        result[f"{col}_M"] = result[col].str.split("-", expand=True).loc[:, 1].astype(int)
+class OvertimeStastics:
+    """Overtime Stastics"""
 
-    result = result.drop(en_cols[4:], axis="columns")
+    SORT_KEY = {
+        # units
+        "署長室": 1,
+        "陳副署長室": 2,
+        "林副署長室": 3,
+        "主任秘書室": 4,
+        "政策規劃組": 5,
+        "前瞻政策科": 6,
+        "產業人才科": 7,
+        "計畫管理科": 8,
+        "綜合業務科": 9,
+        "通訊傳播組": 10,
+        "基礎環境科": 11,
+        "傳播推廣科": 12,
+        "通訊應用科": 13,
+        "平臺經濟組": 14,
+        "平臺應用科": 15,
+        "平臺治理科": 16,
+        "數位體驗科": 17,
+        "數據經濟科": 18,
+        "新興跨域組": 19,
+        "資安產業科": 20,
+        "全球運籌科": 21,
+        "場域實證科": 22,
+        "地方鏈結科": 23,
+        "數位服務組": 24,
+        "軟體產業科": 25,
+        "轉型輔導科": 26,
+        "整合服務科": 27,
+        "創新應用科": 28,
+        "秘書室": 29,
+        "文書科": 30,
+        "事務科": 31,
+        "人事室": 32,
+        "政風室": 33,
+        "主計室": 34,
+        # jobs
+        "署長": 1,
+        "副署長": 2,
+        "主任秘書": 3,
+        "組長": 4,
+        "副組長": 5,
+        "簡任技正": 6,
+        "簡任視察": 7,
+        "專門委員": 8,
+        "科長": 9,
+        "技正": 10,
+        "視察": 11,
+        "專員": 12,
+        "科員": 13,
+        "助理員": 14,
+        "專案規劃師": 15,
+        "專案分析師": 16,
+        "資安系統分析師": 17,
+    }
+    CH_COlS = [
+        "單位名稱",
+        "職稱",
+        "姓名",
+        "加班日期",
+        "核可時數",
+        "已請款時數",
+        "已補休時數",
+        "剩餘可用時數",
+    ]
+    EN_COLS = [
+        "unit",
+        "job",
+        "name",
+        "year-month",
+        "appr",
+        "paid",
+        "rest",
+        "remain",
+    ]
+    CAL_COLS = ["appr", "paid", "rest", "remain"]
+    CAL_COLS_MAP = {
+        "appr-H": "核可(時)",
+        "appr-M": "核可(分)",
+        "paid-H": "請款(時)",
+        "paid-M": "請款(分)",
+        "rest-H": "補休(時)",
+        "rest-M": "補休(分)",
+        "remain-H": "剩餘(時)",
+        "remain-M": "剩餘(分)",
+    }
+    NON_CAL_COLS = ["unit", "job", "name", "year-month"]
 
-    return result
+    def __init__(self, file: str):
+        self.source_df = self._clean(pd.read_excel(file))
 
+    def _clean(self, df: pd.DataFrame):
+        """Clean DataFrame"""
 
-def extract_mons_data(df: pd.DataFrame):
-    result, mons = {}, df["mons"].unique()
-    for mon in mons:
-        inner_df = df[df["mons"] == mon]
-        for col in inner_df.columns[4:]:
-            result = {**result, **{f"{col}_{mon}": inner_df[col].sum()}}
+        # 1) reformat DataFrame
+        column_row: int = df[df.iloc[:, 0] == "單位名稱"].index.values[0]
+        start_row: int = column_row + 1
+        df.columns = df.iloc[column_row, :]
+        df = df.iloc[start_row:][self.CH_COlS].copy()
+        df.rename(columns=dict(zip(self.CH_COlS, self.EN_COLS)), inplace=True)
+        df.columns.name = None
+        df["year-month"] = (
+            df["year-month"]
+            .str.split("/")
+            .map(lambda x: str(int(x[0])) + "-" + str(int(x[1])))
+        )
 
-    return result
+        # 2) split hours and minutes
+        df.fillna("0-0", inplace=True)
+        for col in self.CAL_COLS:
+            df[f"{col}-H"] = df[col].str.split("-").map(lambda x: x[0])
+            df[f"{col}-M"] = df[col].str.split("-").map(lambda x: x[1])
 
+        df.drop(columns=self.CAL_COLS, inplace=True)
 
-def sorted_column_dict(col_patterns) -> dict:
-    columns, result = [], {}
-    for i in range(1, 13):
-        for pattern in col_patterns:
-            columns.append(f"{pattern}_H_{str(i)}")
-            columns.append(f"{pattern}_M_{str(i)}")
+        # 3) sort DataFrame
+        df = df.sort_values(
+            by=["unit", "job"], key=lambda x: x.map(self.SORT_KEY)
+        )
+        df.reset_index(drop=True, inplace=True)
 
-    for i, col in enumerate(columns):
-        result[col] = i
+        return df
 
-    return result
+    def execute(self, file: str = "output.xlsx"):
+        """Execute procedure"""
 
+        personal_data = self._personal_data()
+        result = pd.DataFrame(personal_data).T
+        pd.set_option("future.no_silent_downcasting", True)
+        result.fillna(0, inplace=True)
+        result = self._round_minutes_up(result)
+        result.reset_index(names=["name"], inplace=True)
+        result = result.set_axis(range(1, len(result) + 1))
+        result = self._reformat_columns(result)
+        self._export(result, file)
 
-def integrate_hours_mins(df):
-    result = df.fillna(0).copy(deep=True)
-    # the columns for hours and minutes which need to be integrated.
-    col_patterns = ["appr", "paid", "rest", "remain"]
-    mons = set([col.split("_")[2] for col in result.columns[3:]])
-    for mon in mons:
-        for pattern in col_patterns:
-            hour_col, min_col = f"{pattern}_H_{mon}", f"{pattern}_M_{mon}"
-            result[hour_col] = result[hour_col] + (result[min_col] / 60).astype(int)
-            result[min_col] = (result[min_col] % 60).astype(int)
+        return result
 
-    sort_dict = sorted_column_dict(col_patterns)
-    result = result[list(result.columns[:3]) + sorted(result.columns[3:], key=lambda v: sort_dict.get(v))]
+    def _personal_data(self) -> dict:
+        """
+        Generate personal data dictionary
+        :return: personal data dictionary -> {
+            name: {[year-month]appr_H: time, [year-month]appr_M: time, ...},
+            ...
+        }
+        """
 
-    return result
+        result = {}
+        names = self.source_df["name"].unique()
+        for name in names:
+            name_df = self.source_df[self.source_df["name"] == name].copy()
+            name_df.reset_index(drop=True, inplace=True)
+            # assign personal basic info (unit and job)
+            result[name] = {
+                k: v
+                for k, v in zip(
+                    ["unit", "job"],
+                    [name_df.loc[0, "unit"], name_df.loc[0, "job"]],
+                )
+            }
 
+            result[name].update(self._sumup_year_month_time(name_df))
 
-def aggregate_time(source_df):
-    aggregate_data = {}
-    for name in source_df["name"]:
-        df = source_df[source_df["name"] == name]
-        aggregate_data[name] = {
-            "unit": df["unit"].values[0], "job": df["job"].values[0], "name": df["name"].values[0]}
-        aggregate_data[name] = {**aggregate_data[name], **extract_mons_data(df)}
+        return result
 
-    result = pd.DataFrame.from_dict(data=aggregate_data, orient="index")
-    result.reset_index().rename(columns={"index": "name"}, inplace=True)
-    # the columns for hours and minutes which need to be integrated.
-    result = integrate_hours_mins(result)
-    for col in result.columns[3:]:
-        result[col] = result[col].astype(int)
+    def _sumup_year_month_time(self, df) -> dict:
+        """★ Sum up personal year-month time ★"""
 
-    return result
+        result = {}
+        cal_cols = [col for col in df.columns if col not in self.NON_CAL_COLS]
+        year_month_list = df["year-month"].unique()
+        for y_m in year_month_list:
+            y_m_df = df[df["year-month"] == y_m]
+            for col in cal_cols:
+                y_m_df.loc[:, col] = y_m_df.loc[:, col].astype(int)
+                result = {**result, **{f"[{y_m}]_{col}": y_m_df[col].sum()}}
 
+        return result
 
-# excel part
-def applying_style(ws: Workbook.worksheets, num_rows, num_cols, attributes):
-    if ws.title in attributes:
-        ws["A1"] = "idx"
-        ws.delete_rows(2)
-    for row in range(1, num_rows + 2):
-        for col in range(1, num_cols + 2):
-            ws.cell(row=row, column=col).font = Font(name="Microsoft JhengHei", size=12)
-            ws.cell(row=row, column=col).border = Border(
-                left=Side(style='thin'), right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
-            ws.cell(row=row, column=col).alignment = Alignment(horizontal='center', vertical='center')
+    def _round_minutes_up(self, df):
+        """Round minutes up to hours"""
 
+        result = df.copy()
+        cal_cols = [col for col in df.columns if col not in self.NON_CAL_COLS]
+        year_month_list = set([col.split("_")[0] for col in cal_cols])
+        for y_m in year_month_list:
+            for col in self.CAL_COLS:
+                hour_col, min_col = f"{y_m}_{col}-H", f"{y_m}_{col}-M"
+                result[hour_col] = result[hour_col] + (
+                    result[min_col] / 60
+                ).astype(int)
+                result[min_col] = (result[min_col] % 60).astype(int)
 
-def create_general_sheet(source_df, attributes):
-    wb = Workbook()
-    for attribute in attributes:
-        if attribute == "resource":
-            ws = wb.active
-            ws.title = "resource"
-            df = source_df.copy(deep=True)
-        else:
-            cols = [col for col in source_df.columns if attribute in col]
-            ws = wb.create_sheet(attribute)
-            df = source_df[list(source_df.columns[:3]) + cols].copy(deep=True)
+        return result
 
-        for r in dataframe_to_rows(df, index=True):
-            ws.append(r)
+    def _reformat_columns(self, df: pd.DataFrame):
+        """Reformat columns"""
 
-        applying_style(ws, num_rows=df.shape[0], num_cols=df.shape[1], attributes=attributes)
+        # sort columns
+        basicinfo_cols = ["unit", "job", "name"]
+        cal_cols = [col for col in df.columns if col not in basicinfo_cols]
+        result = df[basicinfo_cols + cal_cols]
 
-    return wb
+        # rename columns
+        # basic info
+        result.rename(
+            columns={"unit": "單位", "job": "職稱", "name": "姓名"},
+            inplace=True,
+        )
+        # calulation info
+        for col in cal_cols:
+            y_m, cal_attr = col.split("_")[0], col.split("_")[1]
+            result.rename(
+                columns={col: f"{y_m}\n{self.CAL_COLS_MAP[cal_attr]}"},
+                inplace=True,
+            )
 
+        return result
 
-def create_single_sheet(source_df: pd.DataFrame, wb: Workbook, names: list, attributes: list):
-    # noinspection PyShadowingNames
-    def insert_metadata(ws, df):
-        for col in reversed(source_df.columns[:3]):
-            ws.insert_cols(1)
-            ws["A1"] = df.loc[0, col]
+    def _export(self, df: pd.DataFrame, file: str):
+        """Export DataFrame to Excel file"""
 
-        ws.merge_cells("A1:A8")
-        ws.merge_cells("B1:B8")
-        ws.merge_cells("C1:C8")
+        sheets = ["原始資料", "核可", "請款", "補休", "剩餘"]
 
-    for name in names:
-        # print(source_df["name"])
-        if name in list(source_df["name"]):
-            ws: Workbook.worksheets = wb.create_sheet(name)
-            df = source_df[source_df["name"] == name].copy(deep=True)
-            df.reset_index(drop=True, inplace=True)
-            i_row, cols = 1, []
-            for attribute in attributes[1:]:
-                cols = [col for col in source_df.columns if attribute in col]
-                for j_col, col in enumerate(cols):
-                    ws.cell(row=i_row, column=j_col + 1).value = col
-                    ws.cell(row=i_row + 1, column=j_col + 1).value = df.loc[0, col]
+        with xw.App(visible=False) as app:
+            wb = app.books.add()
+            for sheet in sheets:
+                ws = wb.sheets.add(sheet, after=wb.sheets[-1])
+                if sheet == "原始資料":
+                    export_df = df.copy()
+                else:
+                    cols = [col for col in df.columns if sheet in col]
+                    export_df = df[list(df.columns[:3]) + cols].copy()
 
-                i_row += 2
+                ws.range("A1").value = export_df
+                ws.range("A1").value = "序號"
+                # style setting
+                ws.range("A1").expand(
+                    "table"
+                ).api.VerticalAlignment = VAlign.xlVAlignCenter
+                ws.range("A1").expand(
+                    "table"
+                ).api.HorizontalAlignment = HAlign.xlHAlignCenter
+                ws.autofit(axis="columns")
 
-            insert_metadata(ws, df)
-            applying_style(ws, num_rows=7, num_cols=len(cols) + 2, attributes=attributes)
-        else:
-            print(f"{name} 不在你家上班~")
-
-
-def to_xlxs_file(source_df: pd.DataFrame, names: list):
-    attributes = ["resource", "appr", "paid", "rest", "remain"]
-    wb = create_general_sheet(source_df, attributes)
-    if names:
-        create_single_sheet(source_df, wb, names, attributes)
-
-    wb.save("超時服勤時數統計表.xlsx")
-
-
-# main execution
-def overtime_statistics(file_name, sort_key, names=None):
-    if names is None:
-        names = []
-    df = pd.read_excel(file_name)
-    result = aggregate_time(clean_data(df))
-    result = result.sort_values(by=["unit", "job"], key=lambda x: x.map(sort_key))
-    result = result.reset_index(drop=True)
-    to_xlxs_file(result, names)
-
-    return result
+            wb.sheets["工作表1"].delete()  # delete default sheet
+            wb.sheets["原始資料"].activate()
+            wb.save("output.xlsx")
+            wb.close()
